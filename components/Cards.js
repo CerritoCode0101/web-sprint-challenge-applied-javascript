197a9915c333a398fb8575b8463e0c24947063ef
CerritoCode0101 <CaseyCerrito@gmail.com>
2020-10-30 11:31:09 -0500

    finished

--- components/Cards.js
+++ components/Cards.js
@@ -20,3 +20,54 @@
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+
+const cardContainer = document.querySelector("div.cards-container");
+
+axios
+.get('https://lambda-times-api.herokuapp.com/articles')
+.then((response) => {
+    let articleCard = response.data.articles;
+    console.log(articleCard);
+
+    Object.entries(articleCard).forEach((articleloop) => {
+      console.log(articleloop[1]);
+      articleloop[1].forEach((article) =>
+        cardContainer.appendChild(cardMaker(article))
+      );
+    });
+  }).catch((err) => {
+    console.log(err);
+  });
+
+
+
+
+
+function cardMaker(obj){
+    const div = document.createElement('div');
+    const div2 = document.createElement('div');
+    const div3 = document.createElement('div');
+    const div4 = document.createElement('div');
+    const img = document.createElement('img');
+    const span = document.createElement('span');
+
+    div.className = "card";
+    div2.className = "headline";
+    div3.className = "author";
+    div4.className = "img-container";
+
+    div.append(div2, div3);
+    div3.append(div4, span);
+    div4.appendChild(img);
+
+    div2.textContent = obj.headline;
+    div3.textContent = obj.authorName;
+    img.src = obj.authorPhoto;
+
+    div2.addEventListener('click',function(){
+        console.log(div2.textContent)
+    } )
+
+    return div
+
+}
